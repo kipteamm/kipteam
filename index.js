46ea40b0ce8ea2c24d74c81d280143d4016017ea
kipteam <toro.een@gmail.com>
2023-11-01 16:39:43 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const moreProjects = document.getElementById('more-projects');
+
+function toggleProjects(button) {
+    moreProjects.classList.toggle('h-full');
+
+    if (button.innerText === 'More') {
+        button.innerText = 'Less';
+    } else {
+        button.innerText = 'More';
+    }
+}
+
+let currentStage = 1;
+
+function setStage(button, stage) {
+    document.getElementById(`stage-${currentStage}`).classList.remove('flex');
+    document.getElementById(`stage-${currentStage}`).classList.add('hidden');
+    document.getElementById(`stage-${stage}`).classList.remove('hidden');
+    document.getElementById(`stage-${stage}`).classList.add('flex');
+
+    currentStage = stage;
+
+    document.querySelector('.active-stage').classList.remove('active-stage');
+
+    button.querySelector('div').classList.add('active-stage')
+}
+
+const samples = document.getElementById('samples');
+let samplesShown = false
+
+function toggleSamples() {
+    if (samplesShown) {
+        samples.classList.remove('h-auto')
+
+        samplesShown = false
+
+        return
+    }
+
+    samples.classList.add('h-auto')
+
+    samplesShown = true
+}
